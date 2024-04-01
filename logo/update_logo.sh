18119b73c4136373e9e3fa993e0ece20990313d4
Julien Duponchelle <julien@duponchelle.info>
2024-04-01 20:58:41 +0200

    Update logo

---
+++ logo/update_logo.sh
@@ -0,0 +1,5 @@
+# Convert the logo to the relevant sizes and formats.
+
+convert -resize 32x32 logo/logo.png colvert/ui/static/favicon.png
+convert -resize 70x70 logo/logo.png colvert/ui/static/logo.png
+
