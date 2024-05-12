f4b55a918d83bba0e285d37596abb6645053e244
Julien Duponchelle <julien@duponchelle.info>
2024-05-12 19:42:18 +0200

    Properly handle error via a SIGTERM when we have a file loading error

--- colvert/cli/daemon.py
+++ colvert/cli/daemon.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import signal
 import webbrowser
 from typing import List
 
@@ -19,7 +20,7 @@ def create_app() -> aiohttp.web.Application:
 
 async def open_browser_schedule(app: aiohttp.web.Application):
     """
-    This is run in the context of the aiohttp event loop. 
+    This is run in the context of the aiohttp event loop.
 
     We need to create a task to open the browser and add it to the background tasks.
     We can directly call webbrowser.open_new_tab(url) because the server is not yet started.
@@ -31,12 +32,14 @@ async def open_browser_schedule(app: aiohttp.web.Application):
 
 
 async def open_browser(app: aiohttp.web.Application):
-    await asyncio.sleep(0.3) # Limit chance a race condition where the server is not yet started
-    
+    await asyncio.sleep(
+        0.3
+    )  # Limit chance a race condition where the server is not yet started
+
     # Call the url with aiohttp.web
     url = f"http://{app['host']}:{app['port']}"
     async with aiohttp.ClientSession() as session:
-        tries = 0 
+        tries = 0
         while True:
             try:
                 async with session.get(url):
@@ -50,6 +53,7 @@ async def open_browser(app: aiohttp.web.Application):
                     logging.error(f"Could not connect to {url}")
                     break
                 await asyncio.sleep(1)
+
 
 @click.command()
 @click.option(
@@ -66,7 +70,7 @@ async def open_browser(app: aiohttp.web.Application):
 @click.option(
     "--table",
     type=str,
-   help="Table to load the file into",
+    help="Table to load the file into",
 )
 @click.argument("files", type=click.File("rb"), nargs=-1)
 def open(port: int, host: str, files: List[click.File], no_browser: bool, table: str):
@@ -75,14 +79,22 @@ def open(port: int, host: str, files: List[click.File], no_browser: bool, table:
     """
     app = create_app()
     app["db"] = Database()
-    logging.info(f"Loading files {', '.join(file.name for file in files)}")
     app["host"] = host
     app["port"] = port
     app["background_tasks"] = set()
     logging.info(f"UI listening on http://{host}:{port}")
-    app.on_startup.append(lambda app: app["db"].load_files([file.name for file in files], table=table))
+
+    async def load_files(app: aiohttp.web.Application):
+        logging.info(f"Loading files {', '.join(file.name for file in files)}")
+        try:
+            await app["db"].load_files([file.name for file in files], table=table)
+        except ValueError as e:
+            logging.error(e)
+            # Send a signal to ourselves to stop the server
+            signal.raise_signal(signal.SIGTERM)
+
+
+    app.on_startup.append(load_files)
     if not no_browser:
         app.on_startup.append(open_browser_schedule)
-    aiohttp.web.run_app(
-        app, host=host, port=port, print=None, access_log=None
-    )
+    aiohttp.web.run_app(app, host=host, port=port, print=None, access_log=None)
