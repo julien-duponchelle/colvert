0dafcbe9fd4906829b0f059861012d1ef1b14ae9
Julien Duponchelle <julien@duponchelle.info>
2024-06-23 11:15:31 +0200

    Create about page with component licenses (#12)

--- colvert/ui/routes/__init__.py
+++ colvert/ui/routes/__init__.py
@@ -2,5 +2,6 @@ from .completion import routes as completion
 from .docs import routes as docs
 from .index import routes as index
 from .results import routes as results
+from .about import routes as about
 
-routes = [*index, *results, *completion, *docs]
+routes = [*index, *results, *completion, *docs, *about]
