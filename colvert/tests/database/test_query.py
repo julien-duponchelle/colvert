4b1cb82a719069179ea88fb9d42038cf2d2cf773
Julien Duponchelle <julien@duponchelle.info>
2024-07-01 22:21:39 +0200

    List all the statement where we need to disable autorun

--- colvert/tests/database/test_query.py
+++ colvert/tests/database/test_query.py
@@ -6,11 +6,20 @@ def test_query_auto_runnable():
     query = Query("SELECT * FROM users")
     assert query.auto_runnable()
 
+    query = Query("SELECT *\nFROM users")
+    assert query.auto_runnable()
+
     # Test when query contains disallowed words
     query = Query("INSERT INTO users (name) VALUES ('John')")
     assert not query.auto_runnable()
 
+    query = Query(query="insert into users (name) VALUES ('John')")
+    assert not query.auto_runnable()
+
     query = Query("UPDATE users SET name = 'John' WHERE id = 1")
+    assert not query.auto_runnable()
+
+    query = Query("UPDATE\nusers SET name = 'John' WHERE id = 1")
     assert not query.auto_runnable()
 
 
